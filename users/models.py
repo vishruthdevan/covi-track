1c843ad13501ea0055b85e0e1bb72917ad6d496c
Vishruth Devan <vishruthdevan@gmail.com>
2021-09-02 20:00:15 +0530

    Create user profile model

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+class UserProfile(models.Model):
+	timestamp = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	
+	address = models.CharField(verbose_name="Address",max_length=100, null=True, blank=True)
+	town = models.CharField(verbose_name="Town/City",max_length=100, null=True, blank=True)
+	state = models.CharField(verbose_name="State",max_length=100, null=True, blank=True)
+	post_code = models.CharField(verbose_name="Post Code",max_length=8, null=True, blank=True)
+	country = models.CharField(verbose_name="Country",max_length=100, null=True, blank=True)
+	longitude = models.CharField(verbose_name="Longitude",max_length=50, null=True, blank=True)
+	latitude = models.CharField(verbose_name="Latitude",max_length=50, null=True, blank=True)
+
+	has_profile = models.BooleanField(default = False)
+	
+	is_active = models.BooleanField(default = True)
+
+	def __str__(self):
+		return f'{self.user}'
