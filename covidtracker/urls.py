fac18dd47f9d36d5d19fdd4d09b184a891443906
Vishruth Devan <vishruthdevan@gmail.com>
2021-09-02 20:02:11 +0530

    Intialize app views and urls

---
+++ covidtracker/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+app_name = 'covidtracker'
+
+urlpatterns = [
+    path('', views.index),
+]
--- covidtracker/views.py
+++ covidtracker/views.py
@@ -1,3 +1,8 @@
+from covidtrackerccda.settings import TEMPLATES
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>Nah he tweakin</h1>")
