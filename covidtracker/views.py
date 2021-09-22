601bc40cb844d45620728cf8f87699e3becd4c45
Vishruth Devan <vishruthdevan@gmail.com>
2021-09-22 16:51:01 +0530

    Render the google map with the javascript api!

--- covidtracker/views.py
+++ covidtracker/views.py
@@ -1,8 +1,10 @@
 from covidtrackerccda.settings import TEMPLATES
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
-    return render(request, "covidtracker/index.html", {})
+    return render(request, "covidtracker/index.html", {"key" : settings.GOOGLE_API_KEY})
