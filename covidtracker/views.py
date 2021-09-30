c59d73a4967edd6e2ea7b4dd57c653cdb0391b51
Vishruth Devan <vishruthdevan@gmail.com>
2021-09-30 23:02:38 +0530

    Send context from index to render map

--- covidtracker/views.py
+++ covidtracker/views.py
@@ -3,8 +3,11 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from users.models import UserProfile
 
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, "covidtracker/index.html", {"key" : settings.GOOGLE_API_KEY})
+    context = {"key" : settings.GOOGLE_API_KEY,
+                "users" : UserProfile.objects.all().exclude(user=request.user)}
+    return render(request, "covidtracker/index.html", context)
