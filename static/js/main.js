d45e8d0a614cb37dd8d5eefaeaae8c6712d8e56a
Vishruth Devan <vishruthdevan@gmail.com>
2021-09-30 23:02:48 +0530

    Multiple markers

--- static/js/main.js
+++ static/js/main.js
@@ -11,12 +11,15 @@ window.initMap = function() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 11.349223, lng: 76.792558},
         zoom: 8,
-      });
-
-    const marker = new google.maps.Marker({
-      position: {lat: 11.349223, lng: 76.792558},
-      map : map,
     });
+
+    var marker, i;
+    for (i = 0; i < coordinates.length; i++) {  
+        marker = new google.maps.Marker({
+             position: new google.maps.LatLng(coordinates[i][0], coordinates[i][1]),
+             map: map
+        });
+    }
 };
 
 document.head.appendChild(script);
