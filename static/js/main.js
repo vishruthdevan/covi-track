5d60851a47402483d0290a5f5a7050cdf194c589
Vishruth Devan <vishruthdevan@gmail.com>
2021-10-02 19:43:19 +0530

    Custom icons on map

--- static/js/main.js
+++ static/js/main.js
@@ -6,20 +6,37 @@ script.async = false;
 function initMap(){}
 
 let map;
-let coordinates = JSON.parse(document.getElementById('coords').textContent);
-console.log(coordinates);
+let coords = JSON.parse(document.getElementById('coords').textContent);
 window.initMap = function() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 11.349223, lng: 76.792558},
         zoom: 8,
     });
 
+    const iconBase =
+    "http://maps.google.com/mapfiles/kml/";
+
+    const icons = {
+        current : iconBase + "pushpin/blue-pushpin.png",
+        zerodose : iconBase + "paddle/orange-circle.png",
+        onedose : iconBase + "paddle/grn-blank.png",
+        twodose : iconBase + "paddle/grn-circle.png",
+        affected : iconBase + "paddle/red-circle.png",
+    }
+    console.log(coords)
     var marker, i;
-    for (i = 0; i < coordinates.length; i++) {  
+    for (i = 1; i < coords.length; i++) {  
         marker = new google.maps.Marker({
-             position: new google.maps.LatLng(coordinates[i][0], coordinates[i][1]),
+             position: new google.maps.LatLng(coords[i].lat, coords[i].lng),
+             icon: {
+                 url: icons[coords[i].type],
+                 scaledSize: new google.maps.Size(25, 25),
+                 origin: new google.maps.Point(0,0),
+                 anchor: new google.maps.Point(0, 0)
+             },
              map: map
         });
+        console.log(marker.icon);
     }
 };
 
