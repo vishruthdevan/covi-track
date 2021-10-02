4bd09dd0c0ce116618168630a6a549e0144afb2d
Vishruth Devan <vishruthdevan@gmail.com>
2021-10-02 19:43:40 +0530

    Add type of user to context for custom icons

--- covidtracker/views.py
+++ covidtracker/views.py
@@ -9,9 +9,19 @@ from users.models import UserProfile
 @login_required
 def index(request):
     coords = list()
-    for i in UserProfile.objects.all().exclude(user=request.user):
-        coords.append([i.latitude, i.longitude])
-    print(coords)
+    for i in UserProfile.objects.all():
+        if request.user == i.user:
+            coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'current'})
+        elif i.covidaffected == 'T':
+            coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'affected'})
+        elif i.covidaffected == 'F' or i.affected == 'R':
+            if i.vaccinated == '0':
+                coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'zerodose'})
+            if i.vaccinated == '1':
+                coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'onedose'})
+            if i.vaccinated == '0':
+                coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'twodose'})
+    
     context = {"key" : settings.GOOGLE_API_KEY,
-                "coords" : coords}
+               "coords" : coords}
     return render(request, "covidtracker/index.html", context)
