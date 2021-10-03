fb02374018074d98820a8ffc75ba59b93a407987
Vishruth Devan <vishruthdevan@gmail.com>
2021-10-03 17:52:57 +0530

    Add filter option for each category

--- covidtracker/views.py
+++ covidtracker/views.py
@@ -9,19 +9,43 @@ from users.models import UserProfile
 @login_required
 def index(request):
     coords = list()
-    for i in UserProfile.objects.all():
-        if request.user == i.user:
-            coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'current'})
-        elif i.covidaffected == 'T':
-            coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'affected'})
-        elif i.covidaffected == 'F' or i.covidaffected == 'R':
-            if i.vaccinated == '0':
-                coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'zerodose'})
-            if i.vaccinated == '1':
-                coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'onedose'})
-            if i.vaccinated == '0':
-                coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'twodose'})
+    category = request.GET.get('category', 'all')
+    print(category)
+    if category == 'all':
+        for i in UserProfile.objects.all():
+            if request.user == i.user:
+                coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'current'})
+            elif i.covidaffected == 'T':
+                coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'affected'})
+            elif i.covidaffected == 'F' or i.covidaffected == 'R':
+                if i.vaccinated == '0':
+                    coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'zerodose'})
+                if i.vaccinated == '1':
+                    coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'onedose'})
+                if i.vaccinated == '2':
+                    coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'twodoses'})
+
+    elif category == 'user':
+        i = UserProfile.objects.get(user=request.user)
+        coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'current'})
     
+    elif category == 'zerodose':
+        for i in UserProfile.objects.filter(vaccinated='0'):
+            coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'zerodose'})
+    
+    elif category == 'onedose':
+        for i in UserProfile.objects.filter(vaccinated='1'):
+            coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'onedose'})
+
+    elif category == 'twodoses':
+        for i in UserProfile.objects.filter(vaccinated='2'):
+            coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'twodoses'})
+
+    elif category == 'affected':
+        for i in UserProfile.objects.filter(covidaffected='T'):
+            coords.append({"lat" : i.latitude, "lng" : i.longitude, 'type' : 'affected'})
+    
+    print(coords)
     context = {"key" : settings.GOOGLE_API_KEY,
                "coords" : coords}
     return render(request, "covidtracker/index.html", context)
--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,6 @@ window.initMap = function() {
         center: {lat: 11.349223, lng: 76.792558},
         zoom: 8,
     });
-
     const iconBase =
     "http://maps.google.com/mapfiles/kml/";
 
@@ -20,19 +19,18 @@ window.initMap = function() {
         current : iconBase + "pushpin/blue-pushpin.png",
         zerodose : iconBase + "paddle/orange-circle.png",
         onedose : iconBase + "paddle/grn-blank.png",
-        twodose : iconBase + "paddle/grn-circle.png",
+        twodoses : iconBase + "paddle/grn-circle.png",
         affected : iconBase + "paddle/red-circle.png",
     }
-    console.log(coords)
     var marker, i;
-    for (i = 1; i < coords.length; i++) {  
+    for (i = 0; i < coords.length; i++) {  
         marker = new google.maps.Marker({
              position: new google.maps.LatLng(coords[i].lat, coords[i].lng),
              icon: {
                  url: icons[coords[i].type],
                  scaledSize: new google.maps.Size(25, 25),
                  origin: new google.maps.Point(0,0),
-                 anchor: new google.maps.Point(0, 0)
+                 //anchor: new google.maps.Point(0, 0)
              },
              map: map
         });
